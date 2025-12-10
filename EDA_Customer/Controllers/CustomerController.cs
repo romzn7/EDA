f9763f40c32b4d03a5f5e4fc544773fac431a99b
romzn7 <rabbz123@gmail.com>
2025-12-11 00:17:51 +0545

    swagger implemented and apis created

---
+++ EDA_Customer/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using EDA_Customer.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EDA_Customer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerDbContext _customerDbContext;
+
+        public CustomerController(CustomerDbContext customerDbContext)
+        {
+            _customerDbContext = customerDbContext;
+        }
+
+        [HttpGet("GetCustomer")]
+        public async Task<IActionResult> GetCustomers()
+        {
+            return Ok(await _customerDbContext.Customers.ToListAsync());
+        }
+
+        [HttpGet("GetProducts")]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await _customerDbContext.Products.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task PostCustomer([FromBody] Customer customer)
+        {
+            await _customerDbContext.Customers.AddAsync(customer);
+            await _customerDbContext.SaveChangesAsync();
+        }
+    }
+}
--- EDA_Customer/Program.cs
+++ EDA_Customer/Program.cs
@@ -1,3 +1,6 @@
+using EDA_Customer.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,14 +8,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<CustomerDbContext>(options =>
+{
+    options.UseSqlite(@"Data Source=customer.db");
+});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<CustomerDbContext>();
+    dbContext.Database.EnsureCreated();
+
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.UseHttpsRedirection();
 
